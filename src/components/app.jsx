3a66d10a79f7a15f6d085a69897754dcfed801f2
leon.poon <szeleung.poon@gmail.com>
2018-03-04 19:54:41 +0800

    added font-awesome (free-brands only for now)

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 export class App extends React.Component {
     render() {
         return <div className="jumbotron">
-            <h1>Hi React is Here!</h1>
+            <h1>Hi React is Here! <i className="fab fa-react"></i></h1>
         </div>;
     }
 }
--- src/vendors.js
+++ src/vendors.js
@@ -3,6 +3,8 @@ import 'jquery';
 import 'react';
 import 'react-dom';
 import 'bootstrap';
+import '@fortawesome/fontawesome';
+import '@fortawesome/fontawesome-free-brands';
 
 import './vendors.css';
 
